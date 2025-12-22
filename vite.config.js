c3ad8878a6bb9a44dc93ec046c9710fda2098483
Sagar1329 <sagargowda1329@gmail.com>
2025-12-22 17:58:49 +0530

    repo name corrected

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
-    base: "/portfoilio-web/", // Replace with your repository name
+    base: "/portfolio-web/",
     plugins: [react()],
 });
